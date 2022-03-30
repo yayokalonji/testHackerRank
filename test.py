039ff5450d1f8ee461cc2ac52d6d5472a8bfb7ad
Jackson Asprilla <con_jasprilla@credicorpbank.com>
2022-03-30 14:58:06 -0500

    Fallow test

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def gradingStudents(grades):
 
     return result
 
-print (gradingStudents([70 ,74, 73, 67, 38, 51, 33]))
+#print (gradingStudents([70 ,74, 73, 67, 38, 51, 33]))
 
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
@@ -30,7 +30,7 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
     print (count_oranges)
 
 
-countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])
+#countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])
 
 def kangaroo(x1, v1, x2, v2):
     if v1 > v2 and (x2 - x1) % (v1 - v2) == 0:
@@ -38,7 +38,7 @@ def kangaroo(x1, v1, x2, v2):
     else:
         print ("NO")
 
-kangaroo(0, 2, 3, 3)
+#kangaroo(0, 2, 3, 3)
 
 # Complete the 'getTotalX' function below.
 #
@@ -56,4 +56,48 @@ def getTotalX(a, b):
         count += left * right
     return count
 
-print(getTotalX([2,4], [16, 32,96]))
+#print(getTotalX([2,4], [16, 32,96]))
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    highest = scores[0]
+    lowest = scores[0]
+    highest_count = 0
+    lowest_count = 0
+    for i in scores:
+        if i > highest:
+            highest = i
+            highest_count += 1
+        if i < lowest:
+            lowest = i
+            lowest_count += 1
+    return highest_count, lowest_count
+
+
+#print(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]))
+
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+def birthday(s, d, m):
+    count = 0
+    for i in range(len(s)):
+        print (s[i:i+m])
+        if sum(s[i:i+m]) == d:
+            count += 1
+    return count
+
+print(birthday([1, 2, 1, 3, 2], 3, 3))
